f94133dd53d80a0bb702ca022be7fc49e8937ced
phucgari <phucgari@gmail.com>
2023-03-11 13:27:55 +0700

    add CRUD to GenericController, add Connector, remove jdbc deppendency

--- src/main/java/controller/GenericController.java
+++ src/main/java/controller/GenericController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import connector.Connector;
+
+import java.util.Collection;
+
 public interface GenericController<E> {
+    String jdbcURL = null;
+    String jdbcUsername = null;
+    String jdbcPassword = null;
+    Connector connector=new Connector(jdbcURL,jdbcUsername,jdbcPassword);
+    Collection<E> showAll();
+    void create();
+    E showByIndex(int index);
+    void update(E Object);
+    void delete(int index);
 }
--- src/main/java/controller/plannedtransaction/PlannedTransactionController.java
+++ src/main/java/controller/plannedtransaction/PlannedTransactionController.java
@@ -3,5 +3,7 @@ package controller.plannedtransaction;
 import controller.GenericController;
 import model.PlannedTransaction;
 
+import java.util.Collection;
+
 public interface PlannedTransactionController extends GenericController<PlannedTransaction> {
 }
