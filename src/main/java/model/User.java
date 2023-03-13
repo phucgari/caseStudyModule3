a37e83d193373c6172cd411d7fff18830af7c003
sutis3p <ngbaoduong1@gmail.com>
2023-03-13 08:36:51 +0700

    yestoday

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -3,26 +3,28 @@ package model;
 public class User {
     private int id;
     private String name;
-    private String password;
+    private String login_name;
+    private String login_password;
     private String email;
     private String picture_url;
     private boolean gender;
-    private String user_dob_date;
-    private String CCCD;
+    private String user_dob;
+    private String card_id;
     private String phone;
 
     public User() {
     }
 
-    public User(int id, String name, String password, String email, String picture_url, boolean gender, String user_dob_date, String CCCD, String phone) {
+    public User(int id, String name, String login_name, String login_password, String email, String picture_url, boolean gender, String user_dob, String card_id, String phone) {
         this.id = id;
         this.name = name;
-        this.password = password;
+        this.login_name = login_name;
+        this.login_password = login_password;
         this.email = email;
         this.picture_url = picture_url;
         this.gender = gender;
-        this.user_dob_date = user_dob_date;
-        this.CCCD = CCCD;
+        this.user_dob = user_dob;
+        this.card_id = card_id;
         this.phone = phone;
     }
 
@@ -42,12 +44,20 @@ public class User {
         this.name = name;
     }
 
-    public String getPassword() {
-        return password;
+    public String getLogin_name() {
+        return login_name;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setLogin_name(String login_name) {
+        this.login_name = login_name;
+    }
+
+    public String getLogin_password() {
+        return login_password;
+    }
+
+    public void setLogin_password(String login_password) {
+        this.login_password = login_password;
     }
 
     public String getEmail() {
@@ -74,20 +84,20 @@ public class User {
         this.gender = gender;
     }
 
-    public String getUser_dob_date() {
-        return user_dob_date;
+    public String getUser_dob() {
+        return user_dob;
     }
 
-    public void setUser_dob_date(String user_dob_date) {
-        this.user_dob_date = user_dob_date;
+    public void setUser_dob(String user_dob) {
+        this.user_dob = user_dob;
     }
 
-    public String getCCCD() {
-        return CCCD;
+    public String getCard_id() {
+        return card_id;
     }
 
-    public void setCCCD(String CCCD) {
-        this.CCCD = CCCD;
+    public void setCard_id(String card_id) {
+        this.card_id = card_id;
     }
 
     public String getPhone() {
