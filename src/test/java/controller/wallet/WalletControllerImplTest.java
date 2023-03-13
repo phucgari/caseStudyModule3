444f26b9747bacb5450e0a94712bc9830aa1607a
phucgari <phucgari@gmail.com>
2023-03-13 11:14:07 +0700

    add 1 more delete testcase

--- src/test/java/controller/wallet/WalletControllerImplTest.java
+++ src/test/java/controller/wallet/WalletControllerImplTest.java
@@ -75,5 +75,10 @@ class WalletControllerImplTest {
                 "Wallet{id=3, name='tiet kiem', balance=1900000, user_id=User{id=2, login_name='popin123', login_password='password123', email='null', picture_url='null', gender=false, user_name='null', user_dob=null, card_id='null', phone='null', address='null'}}, " +
                 "Wallet{id=4, name='dau tu', balance=2200000, user_id=User{id=1, login_name='phuc1', login_password='phuc1password', email='null', picture_url='null', gender=false, user_name='null', user_dob=null, card_id='null', phone='null', address='null'}}, " +
                 "Wallet{id=5, name='tieu vat', balance=100000, user_id=User{id=1, login_name='phuc1', login_password='phuc1password', email='null', picture_url='null', gender=false, user_name='null', user_dob=null, card_id='null', phone='null', address='null'}}]",wallets.toString());
+        walletController.delete(1);
+        wallets= (ArrayList<Wallet>) walletController.showAll();
+        assertEquals("[Wallet{id=3, name='tiet kiem', balance=1900000, user_id=User{id=2, login_name='popin123', login_password='password123', email='null', picture_url='null', gender=false, user_name='null', user_dob=null, card_id='null', phone='null', address='null'}}, " +
+                "Wallet{id=4, name='dau tu', balance=2200000, user_id=User{id=1, login_name='phuc1', login_password='phuc1password', email='null', picture_url='null', gender=false, user_name='null', user_dob=null, card_id='null', phone='null', address='null'}}, " +
+                "Wallet{id=5, name='tieu vat', balance=100000, user_id=User{id=1, login_name='phuc1', login_password='phuc1password', email='null', picture_url='null', gender=false, user_name='null', user_dob=null, card_id='null', phone='null', address='null'}}]",wallets.toString());
     }
 }
