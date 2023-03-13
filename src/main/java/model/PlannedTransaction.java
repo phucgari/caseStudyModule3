5c97c103630c16c5749c5ff0f7848f4d8e8d8ebc
sutis3p <ngbaoduong1@gmail.com>
2023-03-13 08:05:58 +0700

    model

--- src/main/java/model/PlannedTransaction.java
+++ src/main/java/model/PlannedTransaction.java
@@ -1,4 +1,63 @@
 package model;
 
+import java.time.LocalDate;
+
 public class PlannedTransaction extends Transaction{
+    private int id;
+    private String action;
+    private int money_Amount;
+    private User user_id;
+
+    public PlannedTransaction(int id, String action, int money_Amount, User user_id) {
+        this.id = id;
+        this.action = action;
+        this.money_Amount = money_Amount;
+        this.user_id = user_id;
+    }
+
+    public PlannedTransaction(int id, String name, LocalDate time, int money_Amount, String action, Wallet wallet_id, int id1, String action1, int money_Amount1, User user_id) {
+        super(id, name, time, money_Amount, action, wallet_id);
+        this.id = id1;
+        this.action = action1;
+        this.money_Amount = money_Amount1;
+        this.user_id = user_id;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getAction() {
+        return action;
+    }
+
+    @Override
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    @Override
+    public int getMoney_Amount() {
+        return money_Amount;
+    }
+
+    @Override
+    public void setMoney_Amount(int money_Amount) {
+        this.money_Amount = money_Amount;
+    }
+
+    public User getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(User user_id) {
+        this.user_id = user_id;
+    }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,100 @@
 package model;
 
 public class User {
+    private int id;
+    private String name;
+    private String password;
+    private String email;
+    private String picture_url;
+    private boolean gender;
+    private String user_dob_date;
+    private String CCCD;
+    private String phone;
+
+    public User() {
+    }
+
+    public User(int id, String name, String password, String email, String picture_url, boolean gender, String user_dob_date, String CCCD, String phone) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.picture_url = picture_url;
+        this.gender = gender;
+        this.user_dob_date = user_dob_date;
+        this.CCCD = CCCD;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPicture_url() {
+        return picture_url;
+    }
+
+    public void setPicture_url(String picture_url) {
+        this.picture_url = picture_url;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getUser_dob_date() {
+        return user_dob_date;
+    }
+
+    public void setUser_dob_date(String user_dob_date) {
+        this.user_dob_date = user_dob_date;
+    }
+
+    public String getCCCD() {
+        return CCCD;
+    }
+
+    public void setCCCD(String CCCD) {
+        this.CCCD = CCCD;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
