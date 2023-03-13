0cfce8f26d5b130dd70b7a7f0a72ea3f27c6a831
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2023-03-13 16:28:29 +0700

    update CRUD user

--- src/main/java/controller/user/UserControllerImpl.java
+++ src/main/java/controller/user/UserControllerImpl.java
@@ -1,39 +1,21 @@
 package controller.user;
-
-import connector.Connector;
 import model.User;
-
 import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+
 
 public class UserControllerImpl implements UserController{
 
-    private static final String SELECT_ALL_USERS = "select * from users";
+    private static final String SELECT_USER_BY_ID = "select id,login_name, login_password, email, \" +\n" +
+            "            \"picture_url, gender, user_name, user_dob, card_id, phone, address from users where id =?";
     private static final String CREATE_USERS = "INSERT INTO user (login_name, login_password, email, " +
             "picture_url, gender, user_name, user_dob, card_id, phone, address) " +
             "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    private static final String UPDATE_USERS_SQL = "update user set login_name = ?, login_password = ?, email = ?," +
+            "picture_url = ?, gender = ?, user_name = ?, user_dob = ?, card_id = ?, phone = ?, address where id = ?;";
+    private static final String DELETE_USERS_SQL = "delete from user where id = ?;";
     @Override
     public Collection<User> showAll() {
-        try ( Connection connection = connector.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS)) {
-            ResultSet rs = preparedStatement.executeQuery();
-                int id = rs.getInt("id");
-                String login_name = rs.getString("login_name");
-                String login_password = rs.getString("login_password");
-                String email = rs.getString("email");
-                String picture_url = rs.getString("picture_url");
-                boolean gender = rs.getBoolean("gender");
-                String user_name = rs.getString("user_name");
-                String user_dob = rs.getString("user_dob");
-                String card_id = rs.getString("card_id");
-                String phone = rs.getString("phone");
-                String address = rs.getString("address");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
         return null;
     }
 
@@ -61,17 +43,62 @@ public class UserControllerImpl implements UserController{
 
     @Override
     public User showByIndex(int index) {
-        return null;
+        User user = null;
+        try (Connection connection = connector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID)) {
+            preparedStatement.setInt(1, index);
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                String login_name = rs.getString("login_name");
+                String login_password = rs.getString("login_password");
+                String email = rs.getString("email");
+                String picture_url = rs.getString("picture_url");
+                boolean gender = rs.getBoolean("gender");
+                String user_name = rs.getString("user_name");
+                String user_dob = rs.getString("user_dob");
+                String card_id = rs.getString("card_id");
+                String phone = rs.getString("phone");
+                String address = rs.getString("address");
+                user = new User(login_name, login_password, email, picture_url, gender, user_name,
+                        user_dob, card_id, phone, address);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 
 
     @Override
     public void update(User user) {
         //function 2
+        try (Connection connection = connector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USERS_SQL);) {
+            preparedStatement.setString(1, user.getLogin_name());
+            preparedStatement.setString(2, user.getLogin_password());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setString(4, user.getPicture_url());
+            preparedStatement.setBoolean(5, user.isGender());
+            preparedStatement.setString(6, user.getUser_name());
+            preparedStatement.setString(7, user.getUser_dob());
+            preparedStatement.setString(8, user.getCard_id());
+            preparedStatement.setString(9, user.getPhone());
+            preparedStatement.setString(10, user.getAddress());
+            preparedStatement.setInt(11, user.getId());
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(int index) {
-
+        try ( Connection connection = connector.getConnection();
+              PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USERS_SQL);) {
+            preparedStatement.setInt(1, index);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
